74b9e46be079804f8c42717b9682129ab787856c
mattsz <6matheuslima@gmail.com>
2023-03-21 20:43:56 -0300

    add home and age style

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import MovieCard from '../components/MovieCard';
+import './MoviesGrid.css';
 
 const moviesURL: string = import.meta.env.VITE_API;
 const apiKey: string = import.meta.env.VITE_API_KEY;
@@ -19,16 +20,31 @@ const Home = () => {
   }, []);
 
   return (
-    <div className='container'>
-      <h2 className='title'>Melhores Filmes</h2>
-      <div className='movies'>
-        {ratedMovies.length === 0 && <h2>Carregando...</h2>}
-        {ratedMovies.length > 0 && ratedMovies.map((movie) => {
-          const props = { movie: movie, showLink: true };
-          return <MovieCard key={movie.id} props={props} />;
-        })}
+    <>
+      <div className='initialScrren'>
+        <div className='imageBackground'>
+          <h1 className='initialText'>
+            Track films you’ve watched.<br />
+            Save those you want to see.<br />
+            Tell your friends what’s good.
+          </h1>
+          <button className='initialButton'>
+            GET STARTED - IT'S FREE!
+          </button>
+        </div>
+        <div className='fade'></div>
       </div>
-    </div>
+      <div className='container'>
+        <h2 className='title'>Best Rated Movies</h2>
+        <div className='movies-container'>
+          {ratedMovies.length === 0 && <h2>Carregando...</h2>}
+          {ratedMovies.length > 0 && ratedMovies.map((movie) => {
+            const props = { movie: movie, showLink: true };
+            return <MovieCard key={movie.id} props={props} />;
+          })}
+        </div>
+      </div>
+    </>
   );
 }
 
--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import MovieCard from '../components/MovieCard';
+import './MoviesGrid.css';
+
+const searchUrl: string = import.meta.env.VITE_SEARCH;
+const apiKey: string = import.meta.env.VITE_API_KEY;
 
 const Search = () => {
+  const [searchParams] = useSearchParams();
+
+  const [movies, setMovies] = useState<movie[]>([]);
+
+  const valueOfSearch = searchParams.get('q') || '';
+  const query = valueOfSearch.toUpperCase().trim();
+
+  const getSearchedMovies = async (url: string) => {
+    const response = await fetch(url);
+    const data = await response.json();
+    setMovies(data.results);
+  };
+
+  useEffect(() => {  
+    const searchWithQueryUrl: string = `${searchUrl}?${apiKey}&query=${query}`;
+    console.log(searchWithQueryUrl)
+    getSearchedMovies(searchWithQueryUrl);
+  }, [query]);
+
   return (
-    <div>Search</div>
+    <div className='container'>
+      <h3 className='resultSearch'>FOUND X MATCHES FOR "{query}"</h3>
+      <div className='movies-container'>
+        {movies.length === 0 && <h2>Carregando...</h2>}
+        {movies.length > 0 && movies.map((movie) => {
+          const props = { movie: movie, showLink: true };
+          return <MovieCard key={movie.id} props={props} />;
+        })}
+      </div>
+    </div>
   )
 }
 
